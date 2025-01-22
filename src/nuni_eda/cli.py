79ad567cf20c5afb67ce71ab939396ba5391081e
nunininu <sgcho0907@gmail.com>
2025-01-22 11:54:34 +0900

    0.4.0 test_speech, nuni-eda cli 코드 수정

--- src/nuni_eda/cli.py
+++ src/nuni_eda/cli.py
@@ -1,7 +1,7 @@
 from president_speech.db.parquet_interpreter import read_parquet, get_parquet_full_path
 import pandas as pd
 
-def group_by_count(keyword, str, asc: bool = False, rcnt: int = 12):
+def group_by_count(keyword:str, asc: bool = False, rcnt: int = 12):
     """
     지정한 keyword로 필터링하여 대통령별로 그룹화하고, count를 기준으로 정렬하여 데이터프레임으로 반환합니다.
     """
@@ -9,7 +9,7 @@ def group_by_count(keyword, str, asc: bool = False, rcnt: int = 12):
     df = pd.read_parquet(data_path)
     fdf = df[df['speech_text'].str.contains(keyword, case=False)]
     gdf = fdf.groupby("president").size().reset_index(name="count")
-    sdf = gdf.sort_values(by='count', ascending=False).reset_index(drop=True)
+    sdf = gdf.sort_values(by='count', ascending=asc).reset_index(drop=True)
     rdf = sdf.head(rcnt)
     return rdf
 
