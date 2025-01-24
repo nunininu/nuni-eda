460e5be4dcb731dd59a49e82787cebbaf18fc23d
nunininu <sgcho0907@gmail.com>
2025-01-24 12:19:50 +0900

    test ok

--- src/nuni_eda/cli.py
+++ src/nuni_eda/cli.py
@@ -2,7 +2,7 @@ from president_speech.db.parquet_interpreter import read_parquet, get_parquet_fu
 import pandas as pd
 
 
-def add_keyword_count(df: pd.DataFrame, keyword:str) -> pd.DataFrame:
+def add_keyword_count(df: pd.DataFrame, keyword: str) -> pd.DataFrame:
     """
     DataFrame에 keyword_count 컬럼을 추가하여 반환합니다.
     각 speech_text에서 keyword가 등장하는 횟수를 계산합니다.
@@ -10,14 +10,14 @@ def add_keyword_count(df: pd.DataFrame, keyword:str) -> pd.DataFrame:
     # keyword_count 컬럼 추가
     df['keyword_count'] = df['speech_text'].str.count(keyword)
     return df
-   
-def group_by_count(keyword:str, asc: bool = False, rcnt: int = 12, keyword_sum: bool=False):
+
+def group_by_count(keyword: str, asc: bool=False, rcnt: int=12, keyword_sum: bool=False) -> pd.DataFrame:
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
     fdf = df[df['speech_text'].str.contains(keyword, case=False)]
-
+    
     if(keyword_sum):
-        fdf = add_keyword_count(df.copy(), keyword) 
+        fdf = add_keyword_count(fdf.copy(), keyword)
         gdf = fdf.groupby("president").agg(
             count=("speech_text", "size"),  # 연설 개수
             keyword_sum=("keyword_count", "sum")  # keyword 발생 횟수 합산
@@ -26,7 +26,7 @@ def group_by_count(keyword:str, asc: bool = False, rcnt: int = 12, keyword_sum: 
     else:
         gdf = fdf.groupby("president").size().reset_index(name="count")
         sdf = gdf.sort_values(by='count', ascending=asc).reset_index(drop=True)
-
+    
     rdf = sdf.head(rcnt)
     return rdf
 
